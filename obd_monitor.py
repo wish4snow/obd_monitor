484339433bc6a8896031cca189913feeb6ffd3ba
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-02-22 12:21:27 -0600

    supported pids validation now working correct

--- obd_monitor.py
+++ obd_monitor.py
@@ -6,7 +6,7 @@ file = open("obd_car_data.txt", "w+")
 connection = obd.OBD("192.168.0.10", 35000)
 user_state = True
 value_adderess = []
-user_wanted_pids = [0x04, 0x05, 0x0B, 0x0C, 0x11] #range from 0x01 to 0x20 in hex
+user_wanted_pids = [0x02, 0x04, 0x05, 0x0B, 0x0C, 0x11] #range from 0x01 to 0x20 in hex
 print_output = ""
 file.write("New session")
 print ("New session")
@@ -14,10 +14,15 @@ print ("New session")
 supported_pids_mode1 = obd.commands.PIDS_A
 print (connection.query(supported_pids_mode1).value)
 
-for i in range(1, 0x20) :
-	print (i)
-	if ((connection.query(supported_pids_mode1).value[i] == 1) & (i in user_wanted_pids)):
+print(type(connection.query(supported_pids_mode1).value))
+
+for i in range(0x01, 0x21) :
+	print (hex(i))
+	print (connection.query(supported_pids_mode1).value[i - 1] == True)
+	if ((connection.query(supported_pids_mode1).value[i - 1]) & (i in user_wanted_pids)):
 		value_adderess.append(obd.commands[1][i])
+		print ("value added")
+	print ("\n")
 
 while (user_state) :
 	try:
@@ -35,7 +40,12 @@ while (user_state) :
 		time.sleep(1)
 	except KeyboardInterrupt:
 		user_state = False
+		print ("Are you still testing?")
+		
+		if (input(":") == "no"):
+			print ("\nConnection closed")
+			connection.close()
+			file.close()
 
-print ("\nConnection closed")
-connection.close()
-file.close()
+
+
