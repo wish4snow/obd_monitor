d954bc5f9fee2117927f24b63dc3e4cf6b895270
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-03-20 14:10:18 -0500

    working state with new pid checker

--- obd_monitor.py
+++ obd_monitor.py
@@ -8,7 +8,7 @@ file = open("obd_car_data.txt", "w+")
 connection = obd.OBD("192.168.0.10", 35000)
 user_state = True
 value_adderess = []
-user_wanted_pids = [0x02, 0x04, 0x05, 0x0B, 0x0C, 0x11, 0x22] #range from 0x01 to 0x20 in hex
+user_wanted_pids = [0x02, 0x04, 0x05, 0x0B, 0x0C, 0x11, 0x21, 0x06] #range from 0x01 to 0x20 in hex
 print_output = ""
 file.write("New session")
 print ("New session")
@@ -22,14 +22,6 @@ print (connection.query(supported_pids_mode2).value)
 print (connection.query(supported_pids_mode3).value)
 print(type(connection.query(supported_pids_mode1).value))
 
-for i in range(0x01, 0x21) :
-	print (hex(i))
-	print (connection.query(supported_pids_mode1).value[i - 1])
-	if ((connection.query(supported_pids_mode1).value[i - 1]) & (i in user_wanted_pids)):
-		value_adderess.append(obd.commands[1][i])
-		print ("value added")
-	print ("\n")
-print("done")
 
 pid_range = 0x20
 full_pid_bitarray = [True]
@@ -55,6 +47,17 @@ if (full_pid_bitarray[len(full_pid_bitarray) - 1]):
 print (full_pid_bitarray)
 print (len(full_pid_bitarray))
 print("done")
+
+
+for i in range(0x01,len(full_pid_bitarray) - 1) :
+	print (hex(i))
+	#print (connection.query(supported_pids_mode1).value[i - 1])
+	if (full_pid_bitarray[i] & (i in user_wanted_pids)):
+		value_adderess.append(obd.commands[1][i])
+		print ("value added")
+	print ("\n")
+print("done")
+
 while (user_state) :
 	try:
 		
@@ -68,7 +71,7 @@ while (user_state) :
 		print ("values:")
 		print (print_output)
 		print_output = ""
-		time.sleep(1)
+		time.sleep(0.1)
 	except KeyboardInterrupt:
 		user_state = False
 		print ("Are you still testing?")
