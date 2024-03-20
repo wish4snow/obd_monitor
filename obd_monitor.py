d81df3f9346ddd394faed9faa87cb79b32834faf
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-03-20 13:52:32 -0500

    added better way of working with pids

--- obd_monitor.py
+++ obd_monitor.py
@@ -2,28 +2,59 @@ import obd
 import time
 import subprocess, os
 
+print ("Starting monitor...")
+
 file = open("obd_car_data.txt", "w+")
 connection = obd.OBD("192.168.0.10", 35000)
 user_state = True
 value_adderess = []
-user_wanted_pids = [0x02, 0x04, 0x05, 0x0B, 0x0C, 0x11] #range from 0x01 to 0x20 in hex
+user_wanted_pids = [0x02, 0x04, 0x05, 0x0B, 0x0C, 0x11, 0x22] #range from 0x01 to 0x20 in hex
 print_output = ""
 file.write("New session")
 print ("New session")
 
 supported_pids_mode1 = obd.commands.PIDS_A
+supported_pids_mode2 = obd.commands.PIDS_B
+supported_pids_mode3 = obd.commands.PIDS_C
 print (connection.query(supported_pids_mode1).value)
+print (connection.query(supported_pids_mode2).value)
 
+print (connection.query(supported_pids_mode3).value)
 print(type(connection.query(supported_pids_mode1).value))
 
 for i in range(0x01, 0x21) :
 	print (hex(i))
-	print (connection.query(supported_pids_mode1).value[i - 1] == True)
+	print (connection.query(supported_pids_mode1).value[i - 1])
 	if ((connection.query(supported_pids_mode1).value[i - 1]) & (i in user_wanted_pids)):
 		value_adderess.append(obd.commands[1][i])
 		print ("value added")
 	print ("\n")
+print("done")
 
+pid_range = 0x20
+full_pid_bitarray = [True]
+supported_pids_mode1 = obd.commands.PIDS_A
+
+for i in range(0x01, pid_range):
+    full_pid_bitarray.append(connection.query(supported_pids_mode1).value[i - 1])
+
+if (full_pid_bitarray[len(full_pid_bitarray) - 1]):
+
+    supported_pids_mode2 = obd.commands.PIDS_B
+    
+    for i in range(0x00, pid_range + 1):
+    
+        full_pid_bitarray.append(connection.query(supported_pids_mode2).value[i - 1])
+
+if (full_pid_bitarray[len(full_pid_bitarray) - 1]):
+
+    supported_pids_model3 = obd.commands.PIDS_C
+    
+    for i in range(0x00, pid_range):
+        full_pid_bitarray.append(connection.query(supported_pids_mode3).value[i - 1])
+print (full_pid_bitarray)
+print (len(full_pid_bitarray))
+print("done")
 while (user_state) :
 	try:
 		
